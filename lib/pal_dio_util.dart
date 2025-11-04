9ec4dbac17f3a4b0d85bcfbb1b31027f7f4f7d9b
ymsd <ymsd@youmoshidai.com>
2025-11-04 13:55:29 +0800

    fix

--- lib/pal_dio_util.dart
+++ lib/pal_dio_util.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:developer' as developer;
 
 enum ApiMethods {
   get,
@@ -12,19 +13,17 @@ enum ApiMethods {
   delete,
 }
 
-
 class BaseRequest {
   BaseRequest();
 
-  Map<String, dynamic> toJson(){
+  Map<String, dynamic> toJson() {
     return {};
   }
 
-  factory BaseRequest.fromJson(Map<String, dynamic> json){
+  factory BaseRequest.fromJson(Map<String, dynamic> json) {
     return BaseRequest();
   }
 }
-
 
 // Like a json annotation class
 class DummyErrorResponse {
@@ -42,6 +41,25 @@ class DummyErrorResponse {
   }
 }
 
+class PalResponse<T> {
+  int statusCode;
+  dynamic data;
+  Map<String, dynamic>? json;
+  String? error;
+  T? apiData;
+
+  PalResponse({required this.statusCode, this.data, this.error, this.json, this.apiData});
+
+  bool get ok {
+    return statusCode >= 200 && statusCode < 300;
+  }
+
+  @override
+  String toString() {
+    return "[PalResponse]: $statusCode, data: $data, error: $error, json: $json, apiData: $apiData";
+  }
+}
+
 class PalDioUtil {
   PalDioUtil();
 
@@ -51,13 +69,12 @@ class PalDioUtil {
   // Default interceptors
   final List<InterceptorsWrapper> _defaultInterceptors = [
     InterceptorsWrapper(
-      onError: (DioException error, ErrorInterceptorHandler handler){
-        return handler.resolve(error.response!);
-      })
+        onError: (DioException error, ErrorInterceptorHandler handler) {
+      return handler.resolve(error.response!);
+    })
   ];
 
-  void handleException(Map<String, dynamic> exceptionRespData){
-  }
+  void handleException(Map<String, dynamic> exceptionRespData) {}
 
   // Sub class can add their own interceptors here
   @protected
@@ -79,13 +96,13 @@ class PalDioUtil {
 
   // Headers maker hook
   @protected
-  Future<Map<String, dynamic>?> makeHeaders({authRequired = true})async{
+  Future<Map<String, dynamic>?> makeHeaders({authRequired = true}) async {
     return {};
   }
 
   // Dynamic api host hook
   @protected
-  Future<String> getApiHost()async{
+  Future<String> getApiHost() async {
     return "";
   }
 
@@ -95,63 +112,135 @@ class PalDioUtil {
   }
 
   // General api call
-  Future<Map<String, dynamic>?> callApi<REQ extends BaseRequest>(ApiMethods method, String path,
-      { REQ? dataReq, REQ? queriesReq, CancelToken? cancelToken,
-        bool authRequired = true,
-        void Function(int, int)? onReceiveProgress, void Function(int, int)? onSendProgress}) async {
-
+  Future<PalResponse<RESP>> callApi<REQ extends BaseRequest, RESP>(
+      ApiMethods method, String path,
+      {REQ? dataReq,
+      REQ? queriesReq,
+      CancelToken? cancelToken,
+      bool authRequired = true,
+      void Function(int, int)? onReceiveProgress,
+      void Function(int, int)? onSendProgress}) async {
     Options options = await makeOptions(authRequired: authRequired);
     var apiHost = await getApiHost();
     String url = "$apiHost$path";
     Response resp;
 
-    switch (method) {
-      case ApiMethods.get:
-        resp = await dioInstance.get(url,
-            data: dataReq?.toJson(), queryParameters: queriesReq?.toJson(),
-            options: options, cancelToken: cancelToken, onReceiveProgress: onReceiveProgress);
-        break;
-      case ApiMethods.post:
-        resp = await dioInstance.post(url,
-            data: dataReq?.toJson(), queryParameters: queriesReq?.toJson(),
-            options: options, cancelToken: cancelToken, onSendProgress: onSendProgress, onReceiveProgress: onReceiveProgress);
-        break;
-      case ApiMethods.put:
-        resp = await dioInstance.put(url,
-            data: dataReq?.toJson(), queryParameters: queriesReq?.toJson(),
-            options: options, cancelToken: cancelToken, onSendProgress: onSendProgress, onReceiveProgress: onReceiveProgress);
-        break;
-      case ApiMethods.delete:
-        resp = await dioInstance.delete(
-          url,
-          data: dataReq?.toJson(), queryParameters: queriesReq?.toJson(),
-          options: options, cancelToken: cancelToken,);
-        break;
+    PalResponse<RESP> palResp;
+    try {
+      switch (method) {
+        case ApiMethods.get:
+          resp = await dioInstance.get(url,
+              data: dataReq?.toJson(),
+              queryParameters: queriesReq?.toJson(),
+              options: options,
+              cancelToken: cancelToken,
+              onReceiveProgress: onReceiveProgress);
+          break;
+        case ApiMethods.post:
+          resp = await dioInstance.post(url,
+              data: dataReq?.toJson(),
+              queryParameters: queriesReq?.toJson(),
+              options: options,
+              cancelToken: cancelToken,
+              onSendProgress: onSendProgress,
+              onReceiveProgress: onReceiveProgress);
+          break;
+        case ApiMethods.put:
+          resp = await dioInstance.put(url,
+              data: dataReq?.toJson(),
+              queryParameters: queriesReq?.toJson(),
+              options: options,
+              cancelToken: cancelToken,
+              onSendProgress: onSendProgress,
+              onReceiveProgress: onReceiveProgress);
+          break;
+        case ApiMethods.delete:
+          resp = await dioInstance.delete(
+            url,
+            data: dataReq?.toJson(),
+            queryParameters: queriesReq?.toJson(),
+            options: options,
+            cancelToken: cancelToken,
+          );
+          break;
+      }
+      developer.log("resp.data ${resp.data}");
+      palResp = PalResponse(statusCode: resp.statusCode!, data: resp.data);
+    } on DioException catch (e) {
+      String? error;
+      switch (e.type) {
+        case DioExceptionType.connectionTimeout:
+          print("connectionTimeout");
+          // TODO: Handle connection timeout
+          break;
+        case DioExceptionType.sendTimeout:
+          print("sendTimeout");
+          // TODO: Handle send timeout
+          break;
+        case DioExceptionType.receiveTimeout:
+          print("receiveTimeout");
+          // TODO: Handle receive timeout
+          break;
+        case DioExceptionType.badResponse:
+          print("badResponse");
+          // TODO: Handle bad response
+          break;
+        case DioExceptionType.cancel:
+          print("cancel");
+          // TODO: Handle cancel
+          break;
+        case DioExceptionType.unknown:
+          print("unknown");
+          error = "网络错误";
+          // TODO: Handle unknown;
+          break;
+        case DioExceptionType.badCertificate:
+          print("badCertificate");
+          // TODO: Handle bad certificate
+          break;
+        case DioExceptionType.connectionError:
+          print("connectionError");
+          // TODO: Handle connection error
+          break;
+      }
+      palResp = PalResponse(statusCode: 0, error: error);
     }
-
-    var checkedResp = await checkApiResponse(resp);
-    if (checkedResp == null){
-      return null;
-    }else{
-      return await cleanApiResponse(checkedResp);
+    developer.log("resp.data ${palResp.toString()}");
+    var lastResp = await checkApiResponse<RESP>(palResp);
+    // if (checkedResp == null) {
+    //   return null;
+    // } else {
+    //   return await cleanApiResponse(checkedResp);
+    // }
+    if (lastResp.statusCode >= 200 && lastResp.statusCode < 300){
+      lastResp = await cleanApiResponse<RESP>(lastResp);
+    }else if (lastResp.statusCode == 404){
+      lastResp.error = "未找到该资源";
     }
+    // if (lastResp.statusCode == 404){
+    //   lastResp.statusCode = 401;
+    //
+    // }
+    return lastResp;
   }
 
   // Check api response hook
   @protected
-  Future<Response?> checkApiResponse(Response resp)async{
-    if (resp.statusCode! >= 400){
-      // DummyErrorResponse errResp = DummyErrorResponse.fromJson(resp.data);
-      // print("Catch Error Response: ${errResp.toJson()}");
-      handleException(resp.data);
-      return null;
-    }
-    return resp;
+  Future<PalResponse<T>> checkApiResponse<T>(PalResponse<T> palResp) async {
+    // if (palResp.statusCode >= 400) {
+    //   // DummyErrorResponse errResp = DummyErrorResponse.fromJson(resp.data);
+    //   // print("Catch Error Response: ${errResp.toJson()}");
+    //   // handleException(resp.data);
+    //   return null;
+    // }
+    return palResp;
   }
 
   // Clean api response hook
   @protected
-  Future<Map<String, dynamic>> cleanApiResponse(Response resp)async{
-    return resp.data;
+  Future<PalResponse<T>> cleanApiResponse<T>(PalResponse<T> palResp) async {
+    print("${palResp.statusCode}");
+    palResp.json = palResp.data != null ? palResp.data as Map<String, dynamic> : {};
+    return palResp;
   }
 }
